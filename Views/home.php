1672b68b0545ed539b5ada61eb88219fbe90be4f
sofiagalbato <101645003+sofiagalbato@users.noreply.github.com>
2022-10-19 14:42:02 -0300

    fw

--- Views/home.php
+++ Views/home.php
@@ -6,8 +6,10 @@
 <html>
     <head>
         <title>Pet Hero - Home</title>
+        <link href="styles/home.css" rel="stylesheet">
     </head>
+
     <body>
-        
+        <h1> ki</h1>
     </body>
 </html>
