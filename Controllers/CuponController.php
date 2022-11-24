5bed43ff66af30d6487c52321c89d8303abe3140
lucianaabril <lucianaabril702@gmail.com>
2022-11-23 22:32:49 -0300

    cupon

---
+++ Controllers/CuponController.php
@@ -0,0 +1,19 @@
+<?php
+    namespace Controllers;
+    use Models\Cupon as Cupon;
+
+    class CuponController{
+        private $cuponDAO;
+
+        function __contruct(){
+            $this->cuponDAO = new cuponDAO;
+        }
+
+        public  function Add(){
+
+        }
+        
+    }
+
+
+?>
